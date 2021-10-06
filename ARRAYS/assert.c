1e99908dddcb5119f37ad9d5add4ae76375c90be
Jasifmir <88818593+Jasifmir@users.noreply.github.com>
2021-10-06 08:24:37 -0700

    Add files via upload

---
+++ ARRAYS/assert.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdbool.h>
+int main()
+{
+bool x = false;
+x=(2==2);
+printf("%s\n","true","false");
+x=(12==2);
+printf("%s\n","true","false");
+return 0;
+}
